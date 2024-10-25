a71dbf2e2dad1d960ebe0e8789b4c77040d08cd4
Fatih <faatihboz@gmail.com>
2024-10-25 20:36:40 +0300

    character controls implemented

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerController characterController;
+
+    private Rigidbody rb;
+    [SerializeField] private float movementSpeedMultiplier;
+    [SerializeField] private float jumpSpeedMultiplier;
+    [SerializeField] private float fallMultiplier;
+
+    [SerializeField] private float rayLength = 0.5f;
+    [SerializeField] private LayerMask groundLayer;
+
+
+
+    private void Awake()
+    {
+        characterController = new PlayerController();
+        characterController.CharacterMovement.Enable();
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void Start()
+    {
+        characterController.CharacterMovement.Jump.performed += Jump;
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (IsGrounded())
+        {
+            rb.AddForce(jumpSpeedMultiplier * Vector3.up, ForceMode.Impulse);
+        }
+    }
+
+
+    private bool IsGrounded()
+    {
+        Ray ray = new(transform.position, Vector3.down);
+        return Physics.Raycast(ray, rayLength, groundLayer);
+    }
+
+
+    void Move()
+    {
+        Vector2 moveDirection = movementSpeedMultiplier * characterController.CharacterMovement.Movement.ReadValue<Vector2>().normalized;
+
+        rb.linearVelocity = new Vector3(moveDirection.x, rb.linearVelocity.y, moveDirection.y);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+
+}
