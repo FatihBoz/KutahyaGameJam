f885a2cee68b002e74feed4494cf37f4f40dc270
Mustafa Cetin <115427747+mustafa-cetin@users.noreply.github.com>
2024-10-26 23:51:09 +0300

    animasyon

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,14 @@ using UnityEngine;
 [RequireComponent (typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
+    
+[SerializeField]
+    private float rotateSpeedMultiplier=10f;
     [SerializeField] private float movementSpeedMultiplier;
     [SerializeField] private float jumpSpeedMultiplier;
     [SerializeField] private float fallMultiplier;
 
+    [SerializeField] private float speedWhileCarryingMultiplier=0.5f;
     [SerializeField] private float rayLength = 0.5f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float gravityScale;
@@ -22,6 +26,8 @@ public class PlayerMovement : MonoBehaviour
     private float groundCheckRadius;
 
 
+    [SerializeField]
+    private Animator animator;
 
     private PlayerInteract playerInteract;
     private Rigidbody rb;
@@ -30,13 +36,13 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         playerInteract= GetComponent<PlayerInteract>();
+    
     }
     private void Jump()
     {
-        print(CheckGround());
         if(CheckGround())
         {
-
+            animator.SetTrigger("isJumping");
             rb.AddForce(jumpSpeedMultiplier * Vector3.up, ForceMode.Impulse);
         }
     }
@@ -50,10 +56,26 @@ public class PlayerMovement : MonoBehaviour
     void Move()
     {
         Vector2 inputMoveDirection = PlayerInput.Instance.GetMoveDirection();
-        if (playerInteract.GetIsMovementLimited())
+        if (playerInteract.GetIsHoldingObject())
         {
-            inputMoveDirection.x=0;
+            float angle =inputMoveDirection.x*rotateSpeedMultiplier;
+            transform.RotateAround(playerInteract.GetInteractedObjectPosition(),Vector3.up,angle);
+            playerInteract.RotateInteractedObject(angle);
+            rb.linearVelocity = inputMoveDirection.y*playerInteract.GetInteractPoint().forward * movementSpeedMultiplier*speedWhileCarryingMultiplier;
+            if (inputMoveDirection.y!=0)
+            {
+                animator.SetBool("isMoving",true);
+            }
+            else
+            {
+                animator.SetBool("isMoving",false);
+            }
         }
+        else
+        {
+            
+        animator.SetFloat("inputX",inputMoveDirection.x);
+        animator.SetFloat("inputY",inputMoveDirection.y);
 
         Vector2 moveDirection = movementSpeedMultiplier * inputMoveDirection;
  
@@ -62,19 +84,26 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(moveDirection.x, rb.linearVelocity.y, moveDirection.y);
         if (movement.x!=0 || movement.z!=0)
         {
+            animator.SetBool("isMoving",true);
             //transform.forward = new Vector3(movement.x, 0, movement.z); // Hareket y�n�ne g�re rotasyon ayarla
             Quaternion targetRotation = Quaternion.LookRotation(new Vector3(movement.x, 0, movement.z));
             transform.rotation = Quaternion.Euler(-90, targetRotation.eulerAngles.y, targetRotation.eulerAngles.z);
         }
+        else{
+            animator.SetBool("isMoving",false);
+        }
         rb.linearVelocity = movement;
+        
+        }
     }
 
     private void FixedUpdate()
     {
         Vector3 gravity = Physics.gravity * gravityScale;
         rb.AddForce(gravity,ForceMode.Acceleration);
-        Move();
 
+      
+        Move();
         if (rb.linearVelocity.y < 0)
         {
             rb.linearVelocity += (fallMultiplier - 1) * Physics.gravity.y * Time.deltaTime * Vector3.up;
