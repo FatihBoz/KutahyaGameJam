75c4fe311886c63248ff79bd1de50590b50ce9e9
Mustafa Cetin <115427747+mustafa-cetin@users.noreply.github.com>
2024-10-26 11:36:54 +0300

    checkpointmanager test

--- Assets/Scripts/CheckPoint/CheckpointManager.cs
+++ Assets/Scripts/CheckPoint/CheckpointManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CheckpointManager : MonoBehaviour
 {
+    public GameObject playerPrefab;
     public static CheckpointManager Instance; 
     private Vector3 checkpointLoc;
     private bool hasCheckpoint = false;
@@ -12,15 +13,26 @@ public class CheckpointManager : MonoBehaviour
     private void Start()
     {
         checkpointLoc = transform.position;
+        Player.Instance.OnPlayerDied+=SpawnPlayer;
     }
     public void SetCheckpoint(Vector3 position)
     {
         checkpointLoc=position;
+        Debug.Log("Checkpoint "+checkpointLoc+" locaited");
     }
-
     public Vector3 GetCheckpointLocation()
     {
         return checkpointLoc;
     }
+    private void OnDisable() {
+        Player.Instance.OnPlayerDied-=SpawnPlayer;
+    }
+    private void SpawnPlayer()
+    {
+        Debug.Log("spawnlandi");
+        GameObject player = Instantiate(playerPrefab);
+        player.transform.position = checkpointLoc;
+    }
+
 
 }   
