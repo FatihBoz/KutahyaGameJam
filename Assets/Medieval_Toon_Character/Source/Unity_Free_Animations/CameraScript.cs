e753fe7d860924e3fbf561448ca416243fc10837
Mustafa Cetin <115427747+mustafa-cetin@users.noreply.github.com>
2024-10-27 06:19:18 +0300

    Update CameraScript.cs

--- Assets/Medieval_Toon_Character/Source/Unity_Free_Animations/CameraScript.cs
+++ Assets/Medieval_Toon_Character/Source/Unity_Free_Animations/CameraScript.cs
@@ -4,17 +4,36 @@ public class CameraScript : MonoBehaviour
 
 {
     [SerializeField]
-    private Transform player;
+    private Transform center;
     [SerializeField]
     private Vector3 offset;
+    private float rad;
+    private Vector3 dirToCenter;
+    private Vector3 centerVec;
     void Start()
     {
+        transform.position=Player.Instance.transform.position;
         
+        centerVec=center.position;
+        centerVec.y=transform.position.y;
+        
+        dirToCenter=centerVec-transform.position;
+        rad=Vector3.Magnitude(dirToCenter);
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-      transform.position= player.position+offset;  
+        Vector3 realPos= Player.Instance.transform.position+offset;
+
+        centerVec.y=realPos.y;
+        Vector3 fakePos= realPos-centerVec;
+
+        transform.position=centerVec+fakePos.normalized*rad;
+
+        centerVec.y=transform.position.y;
+        transform.LookAt(centerVec);
+        transform.Rotate(new Vector3(0,90f,0));
     }
 }
